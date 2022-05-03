d9180b4d321595edf51427fe2ac6c70b6c28086c
Maksim Troitski <maxim.troitsky@gmail.com>
2022-05-04 01:29:08 +0300

    (job) search updated-desc

--- apps/job/src/main.ts
+++ apps/job/src/main.ts
@@ -1,9 +1,6 @@
 import { writeFileSync, existsSync, mkdirSync } from 'fs';
 import axios from 'axios';
 
-const page = 1;
-const reps = `https://api.github.com/search/repositories?q=angular+in:readme+sort:stars-desc&per_page=100&page=${page}`;
-
 const headers = {
   headers: {
     'user-agent': 'node js',
@@ -11,12 +8,16 @@ const headers = {
   }
 }
 
-function run() {
+function run(page) {
+  console.log('run', page);
+  // const reps = `https://api.github.com/search/repositories?q=angular+in:readme+sort:stars-desc&per_page=100&page=${page}`;
+  const reps = `https://api.github.com/search/repositories?q=angular+in:readme+sort:updated-desc&per_page=100&page=${page}`;
+
   axios.get(reps, headers)
     .then(async (body) => {
       let checked = 0;
 
-      await Promise.all(body.data.items.map(async (repository) => {
+      return Promise.all(body.data.items.map(async (repository) => {
         return axios.get(repository.trees_url.replace('{/sha}', `/${repository.default_branch}`), headers)
           .then(async (tree) => {
             const file = tree.data.tree.find(d => d.path === 'package.json');
@@ -43,8 +44,10 @@ function run() {
                 console.log(`checked: ${++checked}`);
               })
           })
-      })).catch(console.error);
-    })
+      }))
+    }).then(() => {
+      run(page + 1)
+    }).catch(console.error);
 }
 
-run();
+run(1);
